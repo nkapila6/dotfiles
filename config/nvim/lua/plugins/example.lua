8f3a0e0f61d1f348bd6d599e497231ffc2fe530a
Nikhil Kapila <12816113+nkapila6@users.noreply.github.com>
2025-09-12 22:12:31 +0400

    latest backup

--- config/nvim/lua/plugins/example.lua
+++ config/nvim/lua/plugins/example.lua
@@ -138,6 +138,10 @@ return {
     },
   },
 
+  -- {
+  --   "numToStr/Comment.nvim",
+  -- },
+  --
   -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
   -- would overwrite `ensure_installed` with the new value.
   -- If you'd rather extend the default config, use the code below instead:
