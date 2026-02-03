2b52a7ba4bec83d822a5bcb5405e959d7e1e5314
Nikhil Kapila <12816113+nkapila6@users.noreply.github.com>
2026-02-03 13:01:41 +0400

    feat(nvim): updated font

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+--- Search notes by filename
+
+--- obsidian
+vim.keymap.set("n", "<leader>on", "<cmd>ObsidianQuickSwitch<cr>", { desc = "Search Obsidian Notes" })
+
+-- Search text inside notes
+vim.keymap.set("n", "<leader>os", "<cmd>ObsidianSearch<cr>", { desc = "Search Inside Obsidian" })
+
+-- Jump to your "Today" note
+vim.keymap.set("n", "<leader>ot", "<cmd>ObsidianToday<cr>", { desc = "Obsidian Today" })
